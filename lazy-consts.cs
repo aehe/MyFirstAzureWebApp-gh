69d3cf12b331d90eb0ea25ed55125603504e42aa
aehe <aehe@hotmail.com>
2022-03-06 13:18:03 +0700

    test cred scan

---
+++ lazy-consts.cs
@@ -0,0 +1,10 @@
+namespace MyFirstAzureWebApp
+{
+    public class lazy-consts
+    {
+        // A lazy dev has put real credentials into the code base.
+        public static string StorageAccountKey = "AV6PFXi5f4AtrnN5qJVRbT5Dms5Cx1u8IlylqUh7xvJKFmfDffZLX/GICSkqwL/0FKpl9lTts14k1X/dcd4kcQ==";
+        public static string ApiManagementKey = "k6O+lGA7pBJ/3ffjDyeYtKRygmQlWCkbepb9twuQUnYNjJmuOEzs5ZlPzzdcmAePCQ8LbKTS5qgqxHDNUB3pKg==";
+        public static string AzureAdPrincipalSecret = "1rnBak-hX]c@YeFO?4c9zsdaBGtzKyq2";
+    }
+}
