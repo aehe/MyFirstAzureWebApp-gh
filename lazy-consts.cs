52a60a751c6c00c8d5e7a0137eaa845a6f43a0b9
aehe <aehe@hotmail.com>
2022-03-06 13:36:56 +0700

    test lazy cred

--- lazy-consts.cs
+++ lazy-consts.cs
@@ -1,6 +1,6 @@
 namespace MyFirstAzureWebApp
 {
-    public class lazy-consts
+    public class Consts
     {
         // A lazy dev has put real credentials into the code base.
         public static string StorageAccountKey = "AV6PFXi5f4AtrnN5qJVRbT5Dms5Cx1u8IlylqUh7xvJKFmfDffZLX/GICSkqwL/0FKpl9lTts14k1X/dcd4kcQ==";
